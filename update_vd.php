aa62f6ce46fea22944c635f729031933b05961b3
khusain <khusain@hp.(none)>
2013-05-15 18:20:00 +0400

    added offset vd varnishing

--- update_vd.php
+++ update_vd.php
@@ -4,8 +4,10 @@ $vd = array(
     0 => "<option value='0'>Не покрывать</option>",
     1 => "<option value='one matted'>Односторонний матовый</option>",
     2 => "<option value='one glossy'>Односторонний глянцевый</option>",
-    3 => "<option value='two matted'>Двусторонний матовый</option>",
-    4 => "<option value='two glossy'>Двусторонний глянцевый</option>"
+    3 => "<option value='one offset'>Односторонний оффсетный</option>",
+    4 => "<option value='two matted'>Двусторонний матовый</option>",
+    5 => "<option value='two glossy'>Двусторонний глянцевый</option>",
+    6 => "<option value='two offset'>Двусторонний оффсетный</option>"
 );
 //$vd = array(
 //    0 => "Не покрывать<br>",
@@ -24,24 +26,36 @@ if (!isset($_GET["cover"])) { // block
     if ($product == "Sticker" || $product == "Stamping_sticker") {
         echo $vd[1];
         echo $vd[2];
+        echo $vd[3];
     } else if ($common) {
         if ($lamination == "0") {
             if ($product != "Booklet_(termo-glue)" && $product != "Booklet_(brace)" && $product != "Booklet_(spring)") {
                 echo $vd[1];
                 echo $vd[2];
+                echo $vd[3];
             }
             if (!$onesided)
-                echo $vd[3];
-            if (!$onesided)
                 echo $vd[4];
+            if (!$onesided)
+                echo $vd[5];
+            if (!$onesided)
+                echo $vd[6];
         }
         if ($lamination == "one_matted") {
-            if ($product != "Booklet_(brace)" && $product != "Booklet_(spring)") echo $vd[1];
-            if ($product != "Booklet_(brace)" && $product != "Booklet_(spring)") echo $vd[2];
+            if ($product != "Booklet_(brace)" && $product != "Booklet_(spring)")
+                echo $vd[1];
+            if ($product != "Booklet_(brace)" && $product != "Booklet_(spring)")
+                echo $vd[2];
+            if ($product != "Booklet_(brace)" && $product != "Booklet_(spring)")
+                echo $vd[3];
         }
         if ($lamination == "one_glossy") {
-            if ($product != "Booklet_(brace)" && $product != "Booklet_(spring)") echo $vd[1];
-            if ($product != "Booklet_(brace)" && $product != "Booklet_(spring)") echo $vd[2];
+            if ($product != "Booklet_(brace)" && $product != "Booklet_(spring)")
+                echo $vd[1];
+            if ($product != "Booklet_(brace)" && $product != "Booklet_(spring)")
+                echo $vd[2];
+            if ($product != "Booklet_(brace)" && $product != "Booklet_(spring)")
+                echo $vd[3];
         }
     }
 } else { // cover
@@ -50,15 +64,22 @@ if (!isset($_GET["cover"])) { // block
             echo $vd[1];
             echo $vd[2];
             echo $vd[3];
-            echo $vd[4];
+            if (!$onesided)
+                echo $vd[4];
+            if (!$onesided)
+                echo $vd[5];
+            if (!$onesided)
+                echo $vd[6];
         }
         if ($lamination == "one_matted") {
             echo $vd[1];
             echo $vd[2];
+            echo $vd[3];
         }
         if ($lamination == "one_glossy") {
             echo $vd[1];
             echo $vd[2];
+            echo $vd[3];
         }
     }
 }
